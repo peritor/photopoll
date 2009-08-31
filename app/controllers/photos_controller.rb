e5ec3b340499ec0c9b076b56e2a451b6f916df99
Jonathan Weiss <jw@innerewut.de>
2009-08-31 17:41:50 +0200

    remove respond to

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,29 +2,14 @@ class PhotosController < ApplicationController
   def index
     @photos = Photo.all
     @top_photos = Photo.find(:all, :limit => 3, :joins => :ratings, :group => 'ratings.photo_id', :having => 'ratings.value > 3')
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @photos }
-    end
   end
 
   def show
     @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @photo }
-    end
   end
 
   def new
     @photo = Photo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @photo }
-    end
   end
 
   def edit
@@ -34,41 +19,30 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.new(params[:photo])
 
-    respond_to do |format|
-      if @photo.save
-        flash[:notice] = 'Photo was successfully created.'
-        format.html { redirect_to(@photo) }
-        format.xml  { render :xml => @photo, :status => :created, :location => @photo }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
+    if @photo.save
+      flash[:notice] = 'Photo was successfully created.'
+      redirect_to(@photo) 
+    else
+      render :action => "new"
     end
   end
 
   def update
     @photo = Photo.find(params[:id])
 
-    respond_to do |format|
-      if @photo.update_attributes(params[:photo])
-        flash[:notice] = 'Photo was successfully updated.'
-        format.html { redirect_to(@photo) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-      end
-    end
+    if @photo.update_attributes(params[:photo])
+      flash[:notice] = 'Photo was successfully updated.'
+      redirect_to(@photo)
+    else
+      render :action => "edit"
+    end  
   end
 
   def destroy
     @photo = Photo.find(params[:id])
     @photo.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(photos_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(photos_url)
   end
   
   def vote
