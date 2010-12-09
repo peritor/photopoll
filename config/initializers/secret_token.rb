c2b4ad98d69d36e76416deee47795ac0a9db273a
Mathias Meyer <meyer@paperplanes.de>
2010-12-09 17:39:01 +0100

    Update to run on Rails 3.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photopoll::Application.config.secret_token = '60b4dd4b89d833b14c2cf16f4202b06c04d576f7e292efb5d221357aafa402638445459b21de111945cfee99f785edd23af6a7de528fd39a4d58f1c1499f7b23'
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
-ActionController::Routing::Routes.draw do |map|
-  
-  map.resources :photos do |photos|
-    photos.resources :ratings
+Photopoll::Application.routes.draw do
+  resources :photos do
+    resources :ratings
   end
-  
-  map.root :controller => "photos"
-  
-  map.connect '/photos/search/', :controller => 'photos', :action => 'search'
-  
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+
+  match '/' => 'photos#index'
+  match '/photos/search/' => 'photos#search'
+  match '/:controller(/:action(/:id))'
 end
