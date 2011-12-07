8ef849ab8b44efb462b9c8696f8e2ce572c5bc9f
Daniel Hüsch <daniel.huesch@gmail.com>
2011-12-07 18:10:03 +0100

    Change debug output.

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -4,7 +4,7 @@ run "cd #{release_path} && rake stats > /tmp/stats"
   file.puts [Time.now, 'defined?(deploy)', defined?(deploy)].join("\t")
 end
 
-app_deploy = node[:deploy][release_path.split('/')[-2]]
+app_deploy = node[:deploy][release_path.split('/')[-3]]
 ::File.open('/tmp/dhdbg', 'a') do |file|
   file.puts [Time.now, 'app_deploy.class', app_deploy.class].join("\t")
 end
